38f9163aec9027c3c0f5dc0267e16c3dda030142
k-hara <k.hara.pg@gmail.com>
2013-04-04 17:27:28 +0900

    +

--- test/compilable/fwdref8412uni.d
+++ test/compilable/fwdref8412uni.d
@@ -2,7 +2,7 @@
 // PERMUTE_ARGS:
 module fwdref8412uni;
 
-import imports.fwdref8412traits;
+import imports.fwdref8412uni_tab;
 
 template BasicSetOps() {}
 struct RleBitSet(T) { mixin BasicSetOps; }
--- test/compilable/imports/fwdref8412traits.d
+++
@@ -1,3 +0,0 @@
-module imports.fwdref8412traits;
-
-import imports.fwdref8412uni_tab;
