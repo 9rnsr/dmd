b769bd74a2172306106c2faab51fa710cdb8679d
k-hara <k.hara.pg@gmail.com>
2013-09-28 22:24:06 +0900

    ok2

--- test/fail_compilation/fail4269a.d
+++ test/fail_compilation/fail4269a.d
@@ -1,7 +1,17 @@
-enum bool WWW = is(typeof(A.x));
+/*
+TEST_OUTPUT:
+---
+fail_compilation/fail4269a.d(15): Error: undefined identifier B, did you mean interface A?
+fail_compilation/fail4269a.d(15): Error: variable fail4269a.A.blah field not allowed in interface
+fail_compilation/fail4269a.d(16): Error: undefined identifier B, did you mean interface A?
+fail_compilation/fail4269a.d(16): Error: function fail4269a.A.foo function body only allowed in final functions in interface A
+---
+*/
 
-interface A {
+enum bool test = is(typeof(A.x));
+
+interface A
+{
     B blah;
-    void foo(B b){} 
+    void foo(B b) {}
 }
-
