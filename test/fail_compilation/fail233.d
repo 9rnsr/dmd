e8440df96e48043733ae054209763f9e026f2a9b
k-hara <k.hara.pg@gmail.com>
2013-09-30 15:35:29 +0900

    Add `TEST_OUTPUT` in fail207.d to fail299.d

--- test/fail_compilation/fail233.d
+++ test/fail_compilation/fail233.d
@@ -1,10 +1,11 @@
-void bug1176(){
 /*
-Error: void does not have a default initializer
-Error: integral constant must be scalar type, not void
-Error: cannot implicitly convert expression (0) of type int to const(void[])
-Error: cannot cast int to const(void[])
-Error: integral constant must be scalar type, not const(void[])
+TEST_OUTPUT:
+---
+fail_compilation/fail233.d(10): Error: void does not have a default initializer
+---
 */
-        void[1] v;
+
+void bug1176()
+{
+    void[1] v;
 }
