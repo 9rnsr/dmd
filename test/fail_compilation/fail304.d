5085d01aeb9717b7e9ef38cdfa6586bf9df31a23
k-hara <k.hara.pg@gmail.com>
2013-11-26 22:57:22 +0900

    -30x on 265

--- test/fail_compilation/fail304.d
+++ test/fail_compilation/fail304.d
@@ -1,16 +1,7 @@
-/*
-TEST_OUTPUT:
----
-fail_compilation/fail304.d(15): Error: cannot cast from Small to Large
----
-*/
-
 struct Small { uint x; }
 struct Large { uint x, y, z; }
 Small foo() { return Small(); }
-void main()
-{
-    Large l;
-    Small s;
-    l = cast(Large)foo();
+void main() { 
+  Large l; Small s; 
+  l = cast(Large)foo();
 }
