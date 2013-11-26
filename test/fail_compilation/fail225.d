85ef994783df7a4b5b70a000e55cb8cf11753f53
k-hara <k.hara.pg@gmail.com>
2013-11-26 22:42:55 +0900

    +225,233 (they are bad)

--- test/fail_compilation/fail225.d
+++ test/fail_compilation/fail225.d
@@ -2,7 +2,7 @@
 TEST_OUTPUT:
 ---
 fail_compilation/fail225.d(19): Error: cannot implicitly convert expression (1) of type int to immutable(char*)
-fail_compilation/fail225.d(19): Error: too many initializers for Struct
+fail_compilation/fail225.d(19): Error: cannot implicitly convert expression (& ch) of type char* to immutable(char*)
 fail_compilation/fail225.d(22): Error: cannot implicitly convert expression (iStruct2) of type immutable(Struct) to Struct
 fail_compilation/fail225.d(24): Error: cannot implicitly convert expression (& ch) of type char* to immutable(char*)
 ---
