ef9fcee3af96f2acdb9b9ab0451239e56482eb29
k-hara <k.hara.pg@gmail.com>
2013-11-27 13:45:53 +0900

    +Not good (platform dependent)

--- test/fail_compilation/fail280.d
+++ test/fail_compilation/fail280.d
@@ -1,3 +1,10 @@
+/*
+TEST_OUTPUT:
+---
+fail_compilation/fail280.d(18): Error: template instance fail280.t!0 recursive expansion
+---
+*/
+
 // Issue 2920 - recursive templates blow compiler stack
 // template_17_A.
 
--- test/fail_compilation/fail281.d
+++ test/fail_compilation/fail281.d
@@ -1,3 +1,10 @@
+/*
+TEST_OUTPUT:
+---
+fail_compilation/fail281.d(24): Error: template instance fail281.foo!4294967295u recursive expansion
+---
+*/
+
 // Issue 2920 - recursive templates blow compiler stack
 // template_29_B.
 
