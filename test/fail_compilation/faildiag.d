ce08f8f264d7e5bf1bdea6212df168952534422a
k-hara <k.hara.pg@gmail.com>
2013-12-25 10:27:02 +0900

    Zx

--- test/fail_compilation/faildiag.d
+++ test/fail_compilation/faildiag.d
@@ -1,3 +1,17 @@
+/*
+TEST_OUTPUT:
+---
+fail_compilation/faildiag.d(31): Error: function faildiag.foo1 (int) is not callable using argument types (double)
+fail_compilation/faildiag.d(32): Error: function faildiag.foo2 (int) is not callable using argument types (double)
+fail_compilation/faildiag.d(33): Error: function faildiag.foo3()(int).foo3 (int) is not callable using argument types (double)
+fail_compilation/faildiag.d(34): Error: template faildiag.foo4 cannot deduce template function from argument types !()(double), Candidates are:
+fail_compilation/faildiag.d(24):        faildiag.foo4()()
+fail_compilation/faildiag.d(25):        faildiag.foo4(T)(T) if (is(T == int))
+fail_compilation/faildiag.d(35): Error: template faildiag.foo5 cannot deduce template function from argument types !()(double), Candidates are:
+fail_compilation/faildiag.d(27):        faildiag.foo5()()
+---
+*/
+
 void foo1() {}
 void foo1(int) {}
 
