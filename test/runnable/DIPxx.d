1018e48a4c05bd4057882e754fc804da4517bd46
k-hara <k.hara.pg@gmail.com>
2015-09-14 11:25:21 +0900

    fix Issue 15027 - cannot pass arguments of type DirEntry to std.file functions

--- test/runnable/DIPxx.d
+++ test/runnable/DIPxx.d
@@ -57,8 +57,81 @@ static assert(is(int : U, U if isInt));
 static assert(is(int : U, isInt U));
 
 /**********************************/
+// 15027
+
+@property bool empty(T)(T[] a) { return a.length == 0; }
+@property ref T front(T)(T[] a) { return a[0]; }
+void popFront(T)(ref T[] a) { a = a[1..$]; }
+
+@property T[] save(T)(T[] a) { return a; }
+
+@property ref T back(T)(T[] a) { return a[$ - 1]; }
+
+enum bool InputRange(R) =
+is(typeof((ref R r)
+{
+    if (r.empty) {}
+    auto h = r.front;
+    r.popFront();
+}));
+
+
+enum bool ForwardRange(InputRange R) =
+is(typeof((ref R r)
+{
+    static assert(is(typeof({ return r.save; }()) == R));
+}));
+
+enum bool isBidirectionalRange(ForwardRange R) =
+is(typeof((ref R r)
+{
+    r.popBack();
+    static assert(is( typeof({ return r.back; }()) ==
+                      typeof({ return r.front; }()) ));
+}));
+
+// ----
+
+struct DirEntry15027
+{
+    @property string name() { return ""; }
+    alias name this;
+}
+
+bool isDir15027(InputRange R)(R r) { return true; }
+
+void test15027()
+{
+    DirEntry15027 de;
+    bool c = isDir15027(de);
+}
+
+// ----
+
+auto foo15027(ForwardRange R)(R r) { return 1; }
+auto foo15027(  InputRange R)(R r) { return 2; }
+
+struct MyRange15027
+{
+    int[] a;
+    @property empty() { return a.empty; }
+    @property front() { return a.front; }
+    void popFront() { return a.popFront(); }
+    @disable void save();
+}
+
+void test15027a()
+{
+    MyRange15027 r;
+    assert(foo15027(r) == 2);
+}
+
+/**********************************/
 
 int main()
 {
+    test15027();
+    test15027a();
+
     return 0;
 }
