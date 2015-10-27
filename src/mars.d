94fc9effe40d73fcfa458ce8090d0287f89ac982
k-hara <k.hara.pg@gmail.com>
2015-10-27 10:49:55 +0900

    Fix indent level in switch statements

--- src/mars.d
+++ src/mars.d
@@ -654,14 +654,14 @@ Language changes listed by -transition=id:
                         // Bugzilla issue number
                         switch (num)
                         {
-                        case 3449:
-                            global.params.vfield = true;
-                            break;
-                        case 14488:
-                            global.params.vcomplex = true;
-                            break;
-                        default:
-                            goto Lerror;
+                            case 3449:
+                                global.params.vfield = true;
+                                break;
+                            case 14488:
+                                global.params.vcomplex = true;
+                                break;
+                            default:
+                                goto Lerror;
                         }
                     }
                     else if (Identifier.isValidIdentifier(p + 12))
@@ -669,36 +669,36 @@ Language changes listed by -transition=id:
                         const(char)* ident = p + 12;
                         switch (strlen(ident))
                         {
-                        case 3:
-                            if (strcmp(ident, "all") == 0)
-                            {
-                                global.params.vtls = true;
-                                global.params.vfield = true;
-                                global.params.vcomplex = true;
-                                break;
-                            }
-                            if (strcmp(ident, "tls") == 0)
-                            {
-                                global.params.vtls = true;
-                                break;
-                            }
-                            goto Lerror;
-                        case 5:
-                            if (strcmp(ident, "field") == 0)
-                            {
-                                global.params.vfield = true;
-                                break;
-                            }
-                            goto Lerror;
-                        case 7:
-                            if (strcmp(ident, "complex") == 0)
-                            {
-                                global.params.vcomplex = true;
-                                break;
-                            }
-                            goto Lerror;
-                        default:
-                            goto Lerror;
+                            case 3:
+                                if (strcmp(ident, "all") == 0)
+                                {
+                                    global.params.vtls = true;
+                                    global.params.vfield = true;
+                                    global.params.vcomplex = true;
+                                    break;
+                                }
+                                if (strcmp(ident, "tls") == 0)
+                                {
+                                    global.params.vtls = true;
+                                    break;
+                                }
+                                goto Lerror;
+                            case 5:
+                                if (strcmp(ident, "field") == 0)
+                                {
+                                    global.params.vfield = true;
+                                    break;
+                                }
+                                goto Lerror;
+                            case 7:
+                                if (strcmp(ident, "complex") == 0)
+                                {
+                                    global.params.vcomplex = true;
+                                    break;
+                                }
+                                goto Lerror;
+                            default:
+                                goto Lerror;
                         }
                     }
                     else
@@ -718,43 +718,43 @@ Language changes listed by -transition=id:
                 const(char)* path;
                 switch (p[2])
                 {
-                case '-':
-                    global.params.obj = false;
-                    break;
+                    case '-':
+                        global.params.obj = false;
+                        break;
 
-                case 'd':
-                    if (!p[3])
-                        goto Lnoarg;
-                    path = p + 3;
-                    version (Windows)
-                    {
-                        path = toWinPath(path);
-                    }
-                    global.params.objdir = path;
-                    break;
-                case 'f':
-                    if (!p[3])
-                        goto Lnoarg;
-                    path = p + 3;
-                    version (Windows)
-                    {
-                        path = toWinPath(path);
-                    }
-                    global.params.objname = path;
-                    break;
+                    case 'd':
+                        if (!p[3])
+                            goto Lnoarg;
+                        path = p + 3;
+                        version (Windows)
+                        {
+                            path = toWinPath(path);
+                        }
+                        global.params.objdir = path;
+                        break;
+                    case 'f':
+                        if (!p[3])
+                            goto Lnoarg;
+                        path = p + 3;
+                        version (Windows)
+                        {
+                            path = toWinPath(path);
+                        }
+                        global.params.objname = path;
+                        break;
 
-                case 'p':
-                    if (p[3])
+                    case 'p':
+                        if (p[3])
+                            goto Lerror;
+                        global.params.preservePaths = true;
+                        break;
+
+                    case 0:
+                        error(Loc(), "-o no longer supported, use -of or -od");
+                        break;
+
+                    default:
                         goto Lerror;
-                    global.params.preservePaths = true;
-                    break;
-
-                case 0:
-                    error(Loc(), "-o no longer supported, use -of or -od");
-                    break;
-
-                default:
-                    goto Lerror;
                 }
             }
             else if (p[1] == 'D')
@@ -762,23 +762,23 @@ Language changes listed by -transition=id:
                 global.params.doDocComments = true;
                 switch (p[2])
                 {
-                case 'd':
-                    if (!p[3])
-                        goto Lnoarg;
-                    global.params.docdir = p + 3;
-                    break;
+                    case 'd':
+                        if (!p[3])
+                            goto Lnoarg;
+                        global.params.docdir = p + 3;
+                        break;
 
-                case 'f':
-                    if (!p[3])
-                        goto Lnoarg;
-                    global.params.docname = p + 3;
-                    break;
+                    case 'f':
+                        if (!p[3])
+                            goto Lnoarg;
+                        global.params.docname = p + 3;
+                        break;
 
-                case 0:
-                    break;
+                    case 0:
+                        break;
 
-                default:
-                    goto Lerror;
+                    default:
+                        goto Lerror;
                 }
             }
             else if (p[1] == 'H')
@@ -786,23 +786,23 @@ Language changes listed by -transition=id:
                 global.params.doHdrGeneration = true;
                 switch (p[2])
                 {
-                case 'd':
-                    if (!p[3])
-                        goto Lnoarg;
-                    global.params.hdrdir = p + 3;
-                    break;
+                    case 'd':
+                        if (!p[3])
+                            goto Lnoarg;
+                        global.params.hdrdir = p + 3;
+                        break;
 
-                case 'f':
-                    if (!p[3])
-                        goto Lnoarg;
-                    global.params.hdrname = p + 3;
-                    break;
+                    case 'f':
+                        if (!p[3])
+                            goto Lnoarg;
+                        global.params.hdrname = p + 3;
+                        break;
 
-                case 0:
-                    break;
+                    case 0:
+                        break;
 
-                default:
-                    goto Lerror;
+                    default:
+                        goto Lerror;
                 }
             }
             else if (p[1] == 'X')
@@ -810,17 +810,17 @@ Language changes listed by -transition=id:
                 global.params.doJsonGeneration = true;
                 switch (p[2])
                 {
-                case 'f':
-                    if (!p[3])
-                        goto Lnoarg;
-                    global.params.jsonfilename = p + 3;
-                    break;
+                    case 'f':
+                        if (!p[3])
+                            goto Lnoarg;
+                        global.params.jsonfilename = p + 3;
+                        break;
 
-                case 0:
-                    break;
+                    case 0:
+                        break;
 
-                default:
-                    goto Lerror;
+                    default:
+                        goto Lerror;
                 }
             }
             else if (strcmp(p + 1, "ignore") == 0)
@@ -1844,65 +1844,65 @@ extern (C++) void getenv_setargv(const(char)* envvalue, Strings* args)
     {
         switch (*env)
         {
-        case ' ':
-        case '\t':
-            env++;
-            break;
-
-        case 0:
-            return;
-
-        default:
-            args.push(env); // append
-            p = env;
-            slash = 0;
-            instring = 0;
-            c = 0;
-
-            while (1)
-            {
-                c = *env++;
-                switch (c)
-                {
-                case '"':
-                    p -= (slash >> 1);
-                    if (slash & 1)
-                    {
-                        p--;
-                        goto Laddc;
-                    }
-                    instring ^= 1;
-                    slash = 0;
-                    continue;
-
-                case ' ':
-                case '\t':
-                    if (instring)
-                        goto Laddc;
-                    *p = 0;
-                    //if (wildcard)
-                    //    wildcardexpand();     // not implemented
-                    break;
-
-                case '\\':
-                    slash++;
-                    *p++ = c;
-                    continue;
-
-                case 0:
-                    *p = 0;
-                    //if (wildcard)
-                    //    wildcardexpand();     // not implemented
-                    return;
-
-                default:
-                Laddc:
-                    slash = 0;
-                    *p++ = c;
-                    continue;
-                }
+            case ' ':
+            case '\t':
+                env++;
                 break;
-            }
+
+            case 0:
+                return;
+
+            default:
+                args.push(env); // append
+                p = env;
+                slash = 0;
+                instring = 0;
+                c = 0;
+
+                while (1)
+                {
+                    c = *env++;
+                    switch (c)
+                    {
+                        case '"':
+                            p -= (slash >> 1);
+                            if (slash & 1)
+                            {
+                                p--;
+                                goto Laddc;
+                            }
+                            instring ^= 1;
+                            slash = 0;
+                            continue;
+
+                        case ' ':
+                        case '\t':
+                            if (instring)
+                                goto Laddc;
+                            *p = 0;
+                            //if (wildcard)
+                            //    wildcardexpand();     // not implemented
+                            break;
+
+                        case '\\':
+                            slash++;
+                            *p++ = c;
+                            continue;
+
+                        case 0:
+                            *p = 0;
+                            //if (wildcard)
+                            //    wildcardexpand();     // not implemented
+                            return;
+
+                        default:
+                        Laddc:
+                            slash = 0;
+                            *p++ = c;
+                            continue;
+                    }
+                    break;
+                }
         }
     }
 }
@@ -1913,15 +1913,15 @@ extern (C++) void escapePath(OutBuffer* buf, const(char)* fname)
     {
         switch (*fname)
         {
-        case 0:
-            return;
-        case '(':
-        case ')':
-        case '\\':
-            buf.writeByte('\\');
-        default:
-            buf.writeByte(*fname);
-            break;
+            case 0:
+                return;
+            case '(':
+            case ')':
+            case '\\':
+                buf.writeByte('\\');
+            default:
+                buf.writeByte(*fname);
+                break;
         }
         fname++;
     }
--- src/statement.d
+++ src/statement.d
@@ -2274,8 +2274,8 @@ public:
 
         switch (tab.ty)
         {
-        case Tarray:
-        case Tsarray:
+            case Tarray:
+            case Tsarray:
             {
                 if (checkForArgTypes())
                     return this;
@@ -2481,26 +2481,26 @@ public:
                 s = s.semantic(sc);
                 break;
             }
-        case Taarray:
-            if (op == TOKforeach_reverse)
-                warning("cannot use foreach_reverse with an associative array");
-            if (checkForArgTypes())
-                return this;
+            case Taarray:
+                if (op == TOKforeach_reverse)
+                    warning("cannot use foreach_reverse with an associative array");
+                if (checkForArgTypes())
+                    return this;
 
-            taa = cast(TypeAArray)tab;
-            if (dim < 1 || dim > 2)
-            {
-                error("only one or two arguments for associative array foreach");
-                goto Lerror2;
-            }
-            goto Lapply;
-
-        case Tclass:
-        case Tstruct:
-            /* Prefer using opApply, if it exists
-             */
-            if (sapply)
+                taa = cast(TypeAArray)tab;
+                if (dim < 1 || dim > 2)
+                {
+                    error("only one or two arguments for associative array foreach");
+                    goto Lerror2;
+                }
                 goto Lapply;
+
+            case Tclass:
+            case Tstruct:
+                /* Prefer using opApply, if it exists
+                 */
+                if (sapply)
+                    goto Lapply;
             {
                 /* Look for range iteration, i.e. the properties
                  * .empty, .popFront, .popBack, .front and .back
@@ -2657,10 +2657,10 @@ public:
                 error("cannot infer argument types");
                 goto Lerror2;
             }
-        case Tdelegate:
-            if (op == TOKforeach_reverse)
-                deprecation("cannot use foreach_reverse with a delegate");
-        Lapply:
+            case Tdelegate:
+                if (op == TOKforeach_reverse)
+                    deprecation("cannot use foreach_reverse with a delegate");
+            Lapply:
             {
                 if (checkForArgTypes())
                 {
@@ -2866,19 +2866,19 @@ public:
 
                     switch (tn.ty)
                     {
-                    case Tchar:     flag = 0;   break;
-                    case Twchar:    flag = 3;   break;
-                    case Tdchar:    flag = 6;   break;
-                    default:
-                        assert(0);
+                        case Tchar:     flag = 0;   break;
+                        case Twchar:    flag = 3;   break;
+                        case Tdchar:    flag = 6;   break;
+                        default:
+                            assert(0);
                     }
                     switch (tnv.ty)
                     {
-                    case Tchar:     flag += 0;  break;
-                    case Twchar:    flag += 1;  break;
-                    case Tdchar:    flag += 2;  break;
-                    default:
-                        assert(0);
+                        case Tchar:     flag += 0;  break;
+                        case Twchar:    flag += 1;  break;
+                        case Tdchar:    flag += 2;  break;
+                        default:
+                            assert(0);
                     }
                     const(char)* r = (op == TOKforeach_reverse) ? "R" : "";
                     int j = sprintf(fdname.ptr, "_aApply%s%.*s%llu", r, 2, fntab[flag], cast(ulong)dim);
@@ -2978,14 +2978,14 @@ public:
                 s = s.semantic(sc);
                 break;
             }
-        case Terror:
-        Lerror2:
-            s = new ErrorStatement();
-            break;
+            case Terror:
+            Lerror2:
+                s = new ErrorStatement();
+                break;
 
-        default:
-            error("foreach: %s is not an aggregate type", aggr.type.toChars());
-            goto Lerror2;
+            default:
+                error("foreach: %s is not an aggregate type", aggr.type.toChars());
+                goto Lerror2;
         }
         sc.noctor--;
         sc.pop();
@@ -5473,15 +5473,15 @@ public:
 
         switch (tok)
         {
-        case TOKon_scope_exit:
-            *sfinally = s;
-            break;
+            case TOKon_scope_exit:
+                *sfinally = s;
+                break;
 
-        case TOKon_scope_failure:
-            *sexception = s;
-            break;
+            case TOKon_scope_failure:
+                *sexception = s;
+                break;
 
-        case TOKon_scope_success:
+            case TOKon_scope_success:
             {
                 /* Create:
                  *  sentry:   bool x = false;
@@ -5505,8 +5505,8 @@ public:
 
                 break;
             }
-        default:
-            assert(0);
+            default:
+                assert(0);
         }
         return null;
     }
